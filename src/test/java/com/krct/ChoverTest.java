33ce4ef3303599596f6f2fc16a7e9eff31be45a2
Mohan <mohan@13.com>
2026-05-11 21:11:26 +0530

    new change

--- src/test/java/com/krct/ChoverTest.java
+++ src/test/java/com/krct/ChoverTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -23,6 +24,12 @@ public class ChoverTest {
 
     @BeforeMethod
     public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
 
         driver = new ChromeDriver();
 
